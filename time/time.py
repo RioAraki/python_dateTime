033a23e3fefde125a49294d72700304ede15651f
RioAraki <yueee.li@mail.utoronto.ca>
2017-06-26 00:57:54 -0400

    interpret date object in datetime module

--- time/time.py
+++ time/time.py
@@ -22,6 +22,7 @@ time3str1, time3str2 = "Sun 13:00-15:00", "Mon 12:00-21:00"
 # has any notion of time zone, daylight saving time, or other kind of algorithmic or political time adjustment.
 
 # Timedelta object
+print "=====time delta====="
 # A time interval, difference about different times
 d = datetime.timedelta(weeks = 1044, days = 9941, seconds = 1358, microseconds = 1249)
 
@@ -30,4 +31,34 @@ print repr(d)
 print d.total_seconds()
 
 # date object
-#
+print "=====date====="
+# year, month, day; value error if date out of range
+print datetime.date.today()
+
+print datetime.date.fromtimestamp(1498450656)
+# get date from a time stamp
+# timestamp (epoch time): how many seconds from jan 01 1970
+
+d=datetime.date(year=2, month=1, day=1)
+print "d's ordinal: " + str(d.toordinal())
+print "Today's ordinal: " + str(datetime.date.today().toordinal())
+print "From ordinal 10000: " + str(datetime.date.fromordinal(10000))
+# ordinal: 0001 year 01 month 01 day has initial value of 1, one day => 1 value ordinal
+# when compare two dates, transfer to ordinal and compare ordinal value
+
+# date use as dict keys, in boolean -> true
+# other methods: replace(year, month, day)/ timetuple()/ weekday()/ isoweekday()/ isoformat()/ ctime()
+print datetime.date(year=10,month=1,day=1).timetuple()
+print datetime.date(year=10,month=1,day=1).weekday()
+print datetime.date(2004, 1, 4).isocalendar() # 2004/01/04
+# "== (2004, 1, 7) because its the 7th day of first week in 2004"
+# The ISO year consists of 52 or 53 full weeks, and where a week starts on a Monday and ends on a Sunday. The first week
+# of an ISO year is the first (Gregorian) calendar week of a year containing a Thursday. This is called week number 1,
+# and the ISO year of that Thursday is the same as its Gregorian year.
+# For example, 2004 begins on a Thursday, so the first week of ISO year 2004 begins on Monday, 29 Dec 2003 and
+# ends on Sunday, 4 Jan 2004
+print datetime.date(2002, 12, 4).isoformat() #<- default string format of date
+print datetime.date(2002, 12, 4).ctime()
+
+#===important===#
+# strftime(format)/__format__(format)
